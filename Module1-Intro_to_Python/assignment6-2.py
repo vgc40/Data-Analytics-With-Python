52cee534bc0a30c50822755d339afc816be1f85c
Vanessa Garayburu-Caruso <v.garayburucaruso@wsu.edu>
2023-01-14 22:32:17 -0800

    Adding assignment 6 and related files

---
+++ Module1-Intro_to_Python/assignment6-2.py
@@ -0,0 +1,5 @@
+from sys import argv
+
+script, name, age, weight, height = argv
+
+print("{} is {} years old, weighs {} lbs, and is {} tall.".format(name, age, weight, height))
---
+++ Module1-Intro_to_Python/assignment6.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, arg1, arg2, arg3 = argv
+
+print("This script is named {}".format(script))
+print("  The value of argument 1 is: {}", arg1)
+print("  The value of argument 2 is: {}", arg2)
+print("  The value of argument 3 is: {}", arg3)
